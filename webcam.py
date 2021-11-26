69c99fc49cc20033d5b6ffec300422376c67d25d
Light2N <94530644+Light2N@users.noreply.github.com>
2021-11-27 01:04:36 +0700

    Facial Recognition

--- webcam.py
+++ webcam.py
@@ -1,19 +1,25 @@
 import cv2
+import glob
 
+i = len(glob.glob('facial_rec\*.jpg'))
 cam = cv2.VideoCapture(0) # device 0. If not work, try with 1 or 2
+
 
 if not cam.isOpened():
     raise IOError("Cannot open webcam")
 
 while True:
     ret, frame = cam.read()
-    frame = cv2.flip(frame, 1)
-    
+    frame = cv2.flip(frame,2)
+
     cv2.imshow('My App!', frame)
 
     key = cv2.waitKey(1) & 0xFF
     if key==ord("q"):
         break
+    elif key==ord("s"):
+        cv2.imwrite(f'facial_rec\{i:03d}.jpg',frame)
+        i+=1
 
 cam.release()
 cv2.destroyAllWindows()
